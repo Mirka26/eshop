430d34164b9ea114c5f053c2f1fc950cd2950e16
User <userSDAcourse.pl>
2024-01-11 19:45:14 +0100

    index.html

--- store/views.py
+++ store/views.py
@@ -12,27 +12,5 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-# def categories(request):
-#     categories_list = Category.objects.all()
-#     context = {'categories', categories_list}
-#     return render(request, 'categories.html', context)
 
-# def category(request):
-#     category_list = Category.objects.all()
-#     context = {'category': category_list}
-#     return render(request, 'category.html', context)
-#
-#
-# def parameter(request):
-#     parameter_list = Parameter.objects.all()
-#     context = {'parameter': parameter_list}
-#     return render(request, 'parameter.html', context)
-#
-#
-# def accessory(request):
-#     accessory_list = Accessory.objects.all()
-#     context = {'accessory': accessory_list}
-#     return render(request, 'accessory', context)
-#
-#
 
