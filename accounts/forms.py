234af8ad9cb1c735f5d6cf972c50d72bb3f26cd5
User <userSDAcourse.pl>
2024-01-29 14:16:31 +0100

    Profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,35 @@ from store.models import Customer
 
 
 class ProfileForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['birth_date'].widget = forms.DateInput(
+            attrs={
+                'type': 'date',
+                'placeholder': 'dd-mm-yyyy',
+                'class': 'form-control'
+            }
+        )
+
     class Meta:
         model = Customer
         # fields = ["first_name", "last_name", 'address', 'mobile_number', 'gender', 'birth_date', 'avatar']
         fields = "__all__"
+
+        labels = {
+            'first_name': 'Meno',
+            'last_name': 'Priezvisko',
+            'birth_date': 'Datum narodenia',
+            'address': 'Adresa',
+            'email': 'E-mail',
+            'mobile_number': 'Mobilné číslo',
+        }
+
+        help_texts = {
+            'mobile_number': 'Zadejte telefonné číslo vo formáte +421',
+        }
+
+        widgets = {
+            'email': forms.EmailInput(attrs={'placeholder': 'JohnDoe@example.com'}),
+        }
